81f3e5eacf603d319eadc4590daf1c89d8851cef
HrithikMittal <adhikanshmittal@gmail.com>
2019-08-03 15:33:50 +0530

    make the list immutable

--- src/App.js
+++ src/App.js
@@ -14,17 +14,6 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = newName => {
-    //Don't Do this this.state.persons[0].name = "Maximillian";
-    this.setState({
-      persons: [
-        { name: newName, age: "28" },
-        { name: "Manu", age: "29" },
-        { name: "Stephanie", age: "27" }
-      ]
-    });
-  };
-
   nameChangeHandler = event => {
     this.setState({
       persons: [
@@ -33,6 +22,12 @@ class App extends Component {
         { name: "Stephanie", age: "27" }
       ]
     });
+  };
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
   togglePersonHandler = () => {
@@ -55,8 +50,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />;
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+              />
+            );
           })}
         </div>
       );
